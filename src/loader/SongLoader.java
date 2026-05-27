a21861b0cff093a8b2b1688031de0d467a22a13a
Eric Rao <128321795+agathacrispy@users.noreply.github.com>
2026-05-27 15:20:21 -0400

    loading pitches
    SongLoader is able to load pitches from pitches.csv, stored in an array.
    
    Behaves just like LoadLyrics. TBA syncing pitches and displaying in GameplayPanel

--- src/loader/SongLoader.java
+++ src/loader/SongLoader.java
@@ -11,6 +11,7 @@ import java.io.*;
 
 public class SongLoader {
     public ArrayList<LyricLine> lyrics = new ArrayList<LyricLine>();
+    public ArrayList<PitchFrame> pitches = new ArrayList<PitchFrame>();
 
     public void loadLyrics() {
         try
@@ -38,7 +39,23 @@ public class SongLoader {
         {
             System.out.println("can't read");
         }
+    }
 
+    public void loadPitches() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("songs/dangerous-woman/pitches.csv"));
+            String line;
+            br.readLine(); // skip header
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                long ms = Long.parseLong(parts[0].trim());
+                int midi = Integer.parseInt(parts[1].trim());
+                pitches.add(new PitchFrame(ms, midi));
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("can't read pitches");
+        }
     }
 
 }
