f05a76e74ec79fa2dfba43d90cbf069ed5121f22
Eric Rao <128321795+agathacrispy@users.noreply.github.com>
2026-05-27 09:29:51 -0400

    test change

--- src/KaraokeGame.java
+++ src/KaraokeGame.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import javax.swing.SwingUtilities;
 
 public class KaraokeGame {
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Noteworthy");
