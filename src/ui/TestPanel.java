1bf348e79a833ea56f936128f3b51ec7ad274b3b
SptHensley <spthensley@gmail.com>
2026-06-03 12:04:40 -0400

    changing-mic-almost-working-broken-right-now

--- src/ui/TestPanel.java
+++ src/ui/TestPanel.java
@@ -4,6 +4,7 @@ import audio.AudioInputManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Properties;
 
 // class to confirm johnnys audio recording and playback works
 // in GameplayPanel, AIM.startRecording() and AIM.stopRecording() are used - check that class
@@ -14,12 +15,17 @@ public class TestPanel extends JPanel {
     private final JButton recordButton = new JButton("Record");
     private final JButton playButton = new JButton("Play");
 
+    String filePath = "settings.properties";
+    Properties properties = new Properties();
+
     public TestPanel() {
         //setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
 
+        loadProperties();
+
         recordButton.addActionListener(e -> {
             if (!AIM.isRecording()) {
-                AIM.startRecording();
+                AIM.startRecording(properties.getProperty(micDevice));
                 recordButton.setText("Stop");
             } else {
                 AIM.stopRecording();
